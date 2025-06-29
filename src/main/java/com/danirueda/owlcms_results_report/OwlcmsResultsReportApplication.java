dafc566403d362841c6a63b2ddc10ab73f88af49
Dani Rueda <daniR94@gmail.com>
2025-06-29 14:14:48 +0200

    First commit. Project initialized.

---
+++ src/main/java/com/danirueda/owlcms_results_report/OwlcmsResultsReportApplication.java
@@ -0,0 +1,13 @@
+package com.danirueda.owlcms_results_report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OwlcmsResultsReportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OwlcmsResultsReportApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/danirueda/owlcms_results_report/OwlcmsResultsReportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.danirueda.owlcms_results_report;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OwlcmsResultsReportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
