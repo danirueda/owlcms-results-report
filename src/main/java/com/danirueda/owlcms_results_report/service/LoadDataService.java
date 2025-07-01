6dcd805017f480f556f2114dcf5c8a0d0cb2f839
Dani Rueda <daniR94@gmail.com>
2025-07-01 22:04:09 +0200

    Loading entities. Initializing LoadDataServiceTest.

--- src/main/java/com/danirueda/owlcms_results_report/service/LoadDataService.java
+++ src/main/java/com/danirueda/owlcms_results_report/service/LoadDataService.java
@@ -1,17 +1,19 @@
 package com.danirueda.owlcms_results_report.service;
 
+import com.danirueda.owlcms_results_report.model.*;
+import com.danirueda.owlcms_results_report.model.Record;
 import com.google.gson.Gson;
-import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
+import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
-import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.io.Reader;
+import java.lang.reflect.Type;
+import java.util.List;
 
 @Slf4j
 @Service
@@ -20,15 +22,66 @@ public class LoadDataService {
     @Value("${database.path}")
     private String database;
 
+    private final Gson CONVERTER = new Gson();
+
     public void loadDatabase() {
-        Gson gson = new Gson();
         try (JsonReader reader = new JsonReader(new FileReader(database))) {
-            JsonObject config = gson.fromJson(reader, JsonObject.class);
+            JsonObject data = CONVERTER.fromJson(reader, JsonObject.class);
             log.info("Loaded database as JSON Object");
-            log.info(config.toString());
+            log.info(data.toString());
+            List<Athlete> athletes = loadAthletes(data);
+            Competition competition = loadCompetition(data);
+            List<Platform> platforms = loadPlatforms(data);
+            List<Group> groups = loadGroups(data);
+            List<AgeGroup> ageGroups = loadAgeGroups(data);
+            List<Record> records = loadRecords(data);
+            RecordConfig recordConfig = loadRecordConfig(data);
+            log.info("nothing");
         } catch (IOException e) {
             log.error("Error loading database file.", e);
         }
+    }
+
+    protected List<Athlete> loadAthletes(JsonObject data) {
+        Type athleteList = new TypeToken<List<Athlete>>(){}.getType();
+
+        return CONVERTER.fromJson(data.get("athletes"), athleteList);
+    }
+
+    protected Competition loadCompetition(JsonObject data) {
+        Type competition = new TypeToken<Competition>(){}.getType();
+
+        return CONVERTER.fromJson(data.get("competition"), competition);
+    }
+
+    protected List<Platform> loadPlatforms(JsonObject data) {
+        Type platformList = new TypeToken<List<Platform>>(){}.getType();
+
+        return CONVERTER.fromJson(data.get("platforms"), platformList);
+    }
+
+    protected List<Group> loadGroups(JsonObject data) {
+        Type groupList = new TypeToken<List<Group>>(){}.getType();
+
+        return CONVERTER.fromJson(data.get("groups"), groupList);
+    }
+
+    protected List<AgeGroup> loadAgeGroups(JsonObject data) {
+        Type ageGroupList = new TypeToken<List<AgeGroup>>(){}.getType();
+
+        return CONVERTER.fromJson(data.get("ageGroups"), ageGroupList);
+    }
+
+    protected List<Record> loadRecords(JsonObject data) {
+        Type recordList = new TypeToken<List<Record>>(){}.getType();
+
+        return CONVERTER.fromJson(data.get("records"), recordList);
+    }
+
+    protected RecordConfig loadRecordConfig(JsonObject data) {
+        Type recordConfig = new TypeToken<RecordConfig>(){}.getType();
+
+        return CONVERTER.fromJson(data.get("recordConfig"), recordConfig);
     }
 
 }
---
+++ src/test/java/com/danirueda/owlcms_results_report/service/LoadDataServiceTest.java
@@ -0,0 +1,50 @@
+package com.danirueda.owlcms_results_report.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.fail;
+
+@SpringBootTest
+public class LoadDataServiceTest {
+
+    @Autowired
+    private LoadDataService loadDataService;
+
+    @Test
+    public void loadAthletes() {
+        fail("TODO");
+    }
+
+    @Test
+    public void loadCompetition() {
+        fail("TODO");
+    }
+
+    @Test
+    public void loadPlatforms() {
+        fail("TODO");
+    }
+
+    @Test
+    public void loadGroups() {
+        fail("TODO");
+    }
+
+    @Test
+    public void loadAgeGroups() {
+        fail("TODO");
+    }
+
+    @Test
+    public void loadRecords() {
+        fail("TODO");
+    }
+
+    @Test
+    public void loadRecordConfig() {
+        fail("TODO");
+    }
+
+}
